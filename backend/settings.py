31a6cbb108d904dac1a15872c6aae02e7a481b78
sambaf-K <sambadkhatiwada939@gmail.com>
2026-08-31 12:59:36 +0545

    addded the allwwoed origin into the setting.py

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-774td*6c%^5j*+=*7m&09ho^^vpp%*l9rpkna-d#q-02tznfm7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "talentlens-backend-gsai.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
