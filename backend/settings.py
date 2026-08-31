101c706ac2632ef9b95ba487441fbc8d7a5c462d
sambaf-K <sambadkhatiwada939@gmail.com>
2026-08-31 13:15:45 +0545

    added fronted url into backend

--- backend/settings.py
+++ backend/settings.py
@@ -150,7 +150,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5174",
     "http://127.0.0.1:5174",
     "http://localhost:5175",
-    "http://localhost:5176",
+    "https://talentlens-frontend.pages.dev",
 ]
 
 EMAIL_SENDER = config("EMAIL_SENDER")
