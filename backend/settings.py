0a7a28170a4c807e88918898ae30c2a6be1fe302
sambaf-K <sambadkhatiwada939@gmail.com>
2026-08-31 13:04:26 +0545

    fixing the render build error and making all allowed oriign

--- backend/settings.py
+++ backend/settings.py
@@ -26,11 +26,7 @@ SECRET_KEY = 'django-insecure-774td*6c%^5j*+=*7m&09ho^^vpp%*l9rpkna-d#q-02tznfm7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "talentlens-backend-gsai.onrender.com",
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
